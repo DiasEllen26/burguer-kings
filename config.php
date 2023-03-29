8df9301911dfbf1de42e141ce7fb6b367319289f
Ellen Dias <112592384+DiasEllen26@users.noreply.github.com>
2023-03-29 13:23:56 -0300

    Update config.php

--- config.php
+++ config.php
@@ -1,8 +1,7 @@
 <?php
-$servidor = "localhost";
-$usuario = "root";
-$senha = "";
-$banco = "burguerkings";
-
+$servidor = "bohvroi2dwmrx2b0auhb-mysql.services.clever-cloud.com";
+$usuario = "uqbjkaslojfq9rja";
+$senha = "dPskabX9EnIc6h3Dd11b";
+$banco = "bohvroi2dwmrx2b0auhb";
 $con = mysqli_connect($servidor,$usuario,$senha,$banco) or die ("Não foi possível conectar com o banco");
  
