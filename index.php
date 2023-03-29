ceb1f8bf2929d0eba02f123c4b9f68b8dd344507
Ellen Dias <112592384+DiasEllen26@users.noreply.github.com>
2023-03-29 13:51:47 -0300

    Update index.php

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ require "config.php";
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Burger King® </title>
-  <base href="http://localhost:80/burguer-kings/">
+  <base href="https://burguerkings.cleverapps.io/">
   <!-- Bootstrap CSS -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-aFq/bzH65dt+w6FI2ooMVUpc+21e0SRygnTpmBvdBgSdnuTN7QbdgL+OapgHtvPp" crossorigin="anonymous">
   <!-- Bootstrap Icones-->
